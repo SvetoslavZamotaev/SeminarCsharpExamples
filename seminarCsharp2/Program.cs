cf467a1204e1315f7a68cd612bcd30c4cbe73a26
Svetoslav Zamotaev <zamatory1990@gmail.com>
2022-09-17 19:04:36 +0300

    Сделал задание1 семинара1

---
+++ seminarCsharp2/Program.cs
@@ -0,0 +1,24 @@
+﻿//Задача 2: Напишите программу, которая принимает на вход три числа и выдаёт 
+//максимальное из этих чисел.
+
+//2, 3, 7 -> 7
+//44 5 78 -> 78
+//22 3 9 -> 22
+Console.WriteLine("Введите  число A");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите  число B");
+int B = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите  число C");
+int C = Convert.ToInt32(Console.ReadLine());
+if (A > B && A > C)
+{
+    Console.WriteLine(A + " Самое большое число");
+}
+if (B > A && B > C)
+{
+    Console.WriteLine(B + " Самое большое число");
+}
+if (C > A && C > B)
+{
+    Console.WriteLine(C + " Самое большое число");
+}
