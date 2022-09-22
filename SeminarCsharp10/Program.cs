c8acaed023bbc59931d64a4951d1826f3853f2a6
Svetoslav Zamotaev <zamatory1990@gmail.com>
2022-09-22 16:58:18 +0300

    Сделал задания второго семинара кроме 228

---
+++ SeminarCsharp10/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Clear();
+Console.WriteLine("Введите трёхзначное целое число ");
+int number = Convert.ToInt32(Console.ReadLine());
+number = (number / 10) % 10;
+Console.WriteLine("вторая цифра введенного числа равна : " + number);
