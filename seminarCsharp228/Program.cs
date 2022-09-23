a87180a5c5a632dd53f10c22412402b0be1a5e2d
Svetoslav Zamotaev <zamatory1990@gmail.com>
2022-09-23 13:52:56 +0300

    Волшебное задание 228

---
+++ seminarCsharp228/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+Console.WriteLine("Добрый день! введите последовательно 7 цифр , программа вычислит среднее арифметическое");
+int[] numbers = new int[7];
+int index = 0;
+int count = 1;
+int answer = 0;
+while (index < 7)
+{
+    Console.WriteLine("Введите число " + count);
+    numbers[index] = Convert.ToInt32(Console.ReadLine());
+    index++;
+    count++;
+}
+answer = (numbers[0] + numbers[1] + numbers[2] + numbers[3] + numbers[4] + numbers[5] + numbers[6]) / numbers.Length;
+Console.WriteLine("Ответ " + answer);
+
+
+
